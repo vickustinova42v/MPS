b1a4ea6f5571479da8ed7a590d39b8ebca0d3a81
v.ustinova <v.ustinova@involta.ru>
2020-12-03 00:41:38 +0300

    Добавьте файлы проекта.

---
+++ BLL/CrudOperation.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class CrudOperation
+    {
+        PhotoDBEntities bd;
+        public CrudOperation()
+        {
+            bd = new PhotoDBEntities();
+        }
+
+        public List<ProductModel> CheksList()
+        {
+            return bd.Product.ToList().Select(i => new ProductModel(i)).ToList();
+        }
+    }
+}
---
+++ BLL/Models/ProductModel.cs
@@ -0,0 +1,28 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    class ProductModel
+    {
+        public ProductModel() { }
+
+        public ProductModel(Product products)
+        {
+            Id = products.Id;
+            Name = products.Name;
+            Number = products.Number;
+            Cost = products.Cost;
+            Category_FK = products.Category_FK;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public double Cost { get; set; }
+        public int Category_FK { get; set; }
+    }
+}
