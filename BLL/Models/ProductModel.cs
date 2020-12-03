183be1f874f8c387ca524a305a8cc0e2d2bb1fc8
a.aleynikov <a.aleynikov@inuniver.ru>
2020-12-03 18:31:24 +0300

    Categories

--- BLL/Models/ProductModel.cs
+++ BLL/Models/ProductModel.cs
@@ -9,20 +9,25 @@ namespace BLL.Models
 {
     public class ProductModel
     {
-        public ProductModel() { }
-
-        public ProductModel(Product products)
-        {
-            Id = products.Id;
-            Name = products.Name;
-            Number = products.Number;
-            Cost = products.Cost;
-            Category_FK = products.Category_FK;
-        }
         public int Id { get; set; }
         public string Name { get; set; }
         public string Number { get; set; }
         public double Cost { get; set; }
         public int Category_FK { get; set; }
+        public string Category_All_Prod { get; set; }
+
+        public ProductModel() { }
+
+        public ProductModel(Product products, List<CategoryModel> categorys)
+        {
+
+            Id = products.Id;
+            Name = products.Name;
+            Number = products.Number;
+            Cost = products.Cost;
+            Category_FK = products.Category_FK;
+            Category_All_Prod = categorys.Where(i => i.Id == Category_FK).FirstOrDefault().Name;
+            
+        }
     }
 }
