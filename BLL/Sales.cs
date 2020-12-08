9848df8ef3bc3eb4e918b5c55b1a70db562069b3
v.ustinova <v.ustinova@involta.ru>
2020-12-08 20:57:13 +0300

    Abb class of business logic

---
+++ BLL/Sales.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BLL
+{
+    public class Sales
+    {
+        PhotoDBEntities bd;
+        public Sales()
+        {
+            bd = new PhotoDBEntities();
+        }
+
+    }
+}
